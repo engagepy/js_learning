5bf9ca11a91e25f7268eb8c132bc49e02984c933
zora89 <zorawar.purohit@gmail.com>
2023-08-16 03:06:43 +0530

    dogyears added

---
+++ dogYears.js
@@ -0,0 +1,24 @@
+//my age defined as let variable, for it may change ahead
+let myAge = 2;
+
+//early years set
+let earlyYears = 2;
+
+//multiplication assignment operator * used
+earlyYears *= 10.5;
+
+//subtract from my age
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Zorawar'.toLowerCase();
+
+console.log(`My name is ${myName}. I am HUMAN ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
