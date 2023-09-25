0ca38025477aeb8f5947e7fd1e1f7ea775814a7e
zora89 <zorawar.purohit@gmail.com>
2023-09-26 02:16:34 +0530

    higher order functions

--- higher_order_functions.js
+++ higher_order_functions.js
@@ -12,5 +12,30 @@ const addTwo = num => {
     }
   }
   
-  console.log(checkConsistentOutput(addTwo, 6));
+  console.log(checkConsistentOutput(addTwo, 2));
   
+
+  const higherOrderFunc = param => {
+    console.log(param());
+    return `I just invoked ${param.name} as a callback function!`
+  }
+   
+  const anotherFunc = () => {
+    return 'I\'m being invoked by the higher-order function!';
+  }
+  
+  console.log(higherOrderFunc(anotherFunc));
+
+
+
+  /*
+  In this example, we invoked higherOrderFunc() with an anonymous function (a function without a name) that counts to 10. Anonymous functions can be arguments too!
+  */
+  
+  higherOrderFunc(() => {
+    for (let i = 0; i <= 10; i++){
+      console.log(i);
+    }
+  });
+  
+
