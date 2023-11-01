52485fc8e62c4c98a47339fe5c0e3abeedf7e2f2
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2023-11-01 23:26:42 +0530

    new files added

--- dom_element.js
+++ dom_element.js
@@ -5,3 +5,6 @@ document.getElementById('fourth').innerHTML = "Professor Snape";
 document.getElementsByClassName('slytherin')[0].innerHTML = 'Salazar Slytherin';
 
 document.getElementsByTagName('li')[0].innerHTML = 'Dobby'
+
+document.body.style.backgroundColor = '#201F2E';
+document.querySelector('.heading').style.fontFamily = 'Roboto';
