3ee3a43e98b2233b6bdcaa255dfc5133f06e2fd0
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2023-11-07 23:48:35 +0530

    piano project completed

--- piano_project/main.js
+++ piano_project/main.js
@@ -35,13 +35,40 @@ startOver.hidden= true;
 nextOne.onclick = () => {
   nextTwo.hidden = false;
   nextOne.hidden = true;
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('letter-note-six').innerHTML = 'C';
 };
 
 // Write anonymous event handler property and function for the second progress button
-
+nextTwo.onclick = () => {
+  nextThree.hidden = false;
+  nextTwo.hidden = true;
+  document.getElementById('word-five').innerHTML = 'DEAR';
+  document.getElementById('word-six').innerHTML = 'FRI-';
+  lastLyric.style.display = 'inline-block';
+  document.getElementById('letter-note-three').innerHTML = 'G';
+  document.getElementById('letter-note-four').innerHTML = 'E';
+  document.getElementById('letter-note-five').innerHTML = 'C';
+  document.getElementById('letter-note-six').innerHTML = 'B';
+};
 
 // Write anonymous event handler property and function for the third progress button
-
+nextThree.onclick = () => {
+  startOver.hidden = false;
+  nextThree.hidden = true;
+  document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('word-two').innerHTML = 'PY'; 
+  document.getElementById('word-three').innerHTML = 'BIRTH';
+  document.getElementById('word-four').innerHTML = 'DAY';
+  document.getElementById('word-five').innerHTML = 'TO';
+  document.getElementById('word-six').innerHTML = 'YOU'; 
+  document.getElementById('letter-note-one').innerHTML = 'F';
+  document.getElementById('letter-note-two').innerHTML = 'F';
+  document.getElementById('letter-note-three').innerHTML = 'E';
+  document.getElementById('letter-note-four').innerHTML = 'C';
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('letter-note-six').innerHTML = 'C';
+}
 
 // This is the event handler property and function for the startOver button
 startOver.onclick = function() {
@@ -59,4 +86,5 @@ startOver.onclick = function() {
   document.getElementById('letter-note-five').innerHTML = 'C';
   document.getElementById('word-six').innerHTML = 'YOU!';
   document.getElementById('letter-note-six').innerHTML = 'B';
+  lastLyric.style.display = 'none';
 }
